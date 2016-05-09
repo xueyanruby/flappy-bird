fbfaf0f864370aaacbc34ed2b8dd3e91924a8a38
xueyanruby <xueyanruby@163.com>
2016-05-09 15:27:22 +0800

    Added files via upload

---
+++ src/background.js
@@ -0,0 +1,167 @@
+function setBackground()
+{
+  Game.backgroundcanvas = document.createElement("canvas");
+  Game.backgroundcanvas.style.position = Game.canvas.style.position ;
+  Game.backgroundcanvas.style.left = Game.canvas.style.left ;
+  Game.backgroundcanvas.style.top = Game.canvas.style.top ;
+  Game.backgroundcanvas.width = Game.canvas.width ;
+  Game.backgroundcanvas.height = Game.canvas.height ;
+  Game.backgroundcanvas.style.background = "#71c5cf";
+  var cvs = Game.backgroundcanvas.getContext("2d");
+
+//白云
+  cvs.translate(0,Game.canvas.height-200);
+  cvs.fillStyle = "#e9fad8";
+  
+  var maxradius =  80 ;
+  var minradius = 40;
+  var k = 0.6 * minradius;
+  for(var i = k ; i < Game.canvas.width ;i = i+ k)
+  {
+    cvs.beginPath();
+    cvs.arc(i,100,minradius+(maxradius-minradius)*Math.random(), Math.PI,2*Math.PI);
+    cvs.closePath();
+    cvs.fill();
+  }
+  cvs.fillRect(0,80,Game.canvas.width,70);
+
+ //房子
+  var maxheight = 80 ;
+  var minheight = 40 ;
+  var maxwidth = 40;
+  var minwidth = 20 ;
+  var d = 20 ;
+  cvs.translate(0, 70); 
+  cvs.fillStyle = "#deefcb";
+  var w = 0  ;
+  var h = 0 ; 
+  for(var i = 10 ; i < Game.canvas.width ;i=i+d+w)
+  {
+    w = minwidth+(maxwidth-minwidth)*Math.random();
+	h = minheight+(maxheight-minheight)*Math.random();
+    cvs.fillRect(i,(maxheight-h),w,h);
+  }
+  cvs.fillStyle = "#dff1c4";
+  cvs.strokeStyle = "#9fd5d5";
+  cvs.lineWidth = 3 ;
+  d = 10 ;
+  for(var i = 0 ; i < Game.canvas.width ;i=i+d+w)
+  {
+    w = minwidth+(maxwidth-minwidth)*Math.random();
+	h = minheight+(maxheight-minheight)*Math.random();
+    cvs.fillRect(i,(maxheight-h),w,h);
+	cvs.strokeRect(i,(maxheight-h),w,h);
+  }
+ //草坪
+   cvs.fillStyle = "#81e18b";
+   cvs.fillRect(0,80,Game.canvas.width,40);
+
+ //树
+   var maxtreeheight = 50 ;
+   var mintreeheight = 30 ;
+   var maxh = 110 ;
+   var minh = 85 ;
+
+ //沙滩
+   cvs.fillStyle = "#dcd795";
+   cvs.fillRect(0,120,Game.canvas.width,10);
+  Game.canvascontainer.appendChild(Game.backgroundcanvas);
+}
+
+
+function createGround()
+{
+  var x = 3 ;
+  var y = 0 ;
+  var h = Game.canvas.height -260 ;//两根柱子上下间距150
+  var minh = 30 ;
+  var maxh = h-30 ;
+  var th =  minh+(maxh-minh)*Math.random() ;
+  var bh = h-th ;
+  var w = 70 ;
+  Game.groundh  = th ;
+  var groundcanvas = document.createElement("canvas");
+  groundcanvas.style.position = Game.canvas.style.position ;
+  groundcanvas.style.left = Game.canvas.style.left ;
+  groundcanvas.style.top = Game.canvas.style.top ;
+  groundcanvas.width = 70 ;
+  groundcanvas.height = Game.canvas.height ;
+
+  var groundLightGreen = "#97e556";
+  var groundDarkGreen = "#73be29";
+  var groundDarkerGreen = "#4b7e19";
+  var groundBorder = "#4c3f48";
+  var cvs =  groundcanvas.getContext("2d");
+
+  var grd=cvs.createLinearGradient(x,y,x+w,y);
+  grd.addColorStop(0,groundLightGreen);
+  grd.addColorStop(0.1,groundDarkGreen);
+  grd.addColorStop(0.5,groundDarkerGreen);
+  grd.addColorStop(0.9,groundDarkGreen);
+  grd.addColorStop(1, groundLightGreen);
+  cvs.fillStyle=grd;
+  cvs.strokeStyle = groundBorder ;
+  cvs.fillRect(x,y,w-6,th);
+  cvs.strokeRect(x,y,w-6,th);
+  cvs.fillRect(x-3,y+th,w,30);
+  cvs.strokeRect(x-3,y+th,w,30);
+
+
+  cvs.fillRect(x-3,y+th+180,w,30);
+  cvs.strokeRect(x-3,y+th+180,w,30);
+  cvs.fillRect(x,y+th+210,w-6,bh);
+  cvs.strokeRect(x,y+th+210,w-6,bh);
+
+  return groundcanvas ;
+  //Game.canvascontainer.appendChild(Game.groundcanvas);
+}
+
+function createScrollBar()
+{
+	var scrollbarcanvas = document.createElement("canvas");
+
+    scrollbarcanvas.width = Game.canvas.width ;
+    scrollbarcanvas.height = 10 ;
+    var cvs =  scrollbarcanvas.getContext("2d");
+    var groundLightGreen = "#97e556";
+    var groundDarkGreen = "#73be29";
+    var groundDarkerGreen = "#4b7e19";
+	var fillcolor = groundLightGreen
+
+	var w = 5 ;
+	var h = 10 ;
+    for(var x =0 ; x<=Game.canvas.width+2.5*w;x=x+2.5*w)
+	{
+	  var grd=cvs.createLinearGradient(x,0,x, h);
+      grd.addColorStop(0,groundLightGreen);
+      grd.addColorStop(0.5,groundDarkGreen);
+      grd.addColorStop(1,groundLightGreen);
+
+	  cvs.fillStyle = grd ;
+	  cvs.beginPath();
+      cvs.moveTo(x,0);
+	  cvs.lineTo(x+w,0);
+	  cvs.lineTo(x+w/2,h);
+	  cvs.lineTo(x-w/2,h);
+	  cvs.closePath();
+	  cvs.fill();
+
+	  grd=cvs.createLinearGradient(x+w,0,x+w, h);
+      grd.addColorStop(0,groundDarkGreen);
+      grd.addColorStop(0.5,groundDarkerGreen);
+      grd.addColorStop(1,groundDarkGreen);
+	  cvs.fillStyle = grd ;
+	  cvs.beginPath();
+      cvs.moveTo(x+w,0);
+	  cvs.lineTo(x+2.5*w,0);
+	  cvs.lineTo(x+w*2,h);
+	  cvs.lineTo(x+w/2,h);
+	  cvs.closePath();
+	  cvs.fill();
+
+	}
+	 // Game.canvascontainer.appendChild(scrollbarcanvas);
+ 
+	return scrollbarcanvas ;
+}
+
---
+++ src/init.js
@@ -0,0 +1,199 @@
+window.onload = function(){
+  window.Game = new Object();
+  Game.canvascontainer = document.getElementById("canvascontainer");
+  Game.canvas = document.createElement("canvas");
+  Game.canvas.style.position = "absolute" ;
+  Game.canvas.style.left = "0px" ;
+  Game.canvas.style.top = "0px" ;
+  Game.canvas.width = 800 ;
+  Game.canvas.height = 623 ;
+  setBackground();
+
+  Game.groundh = 0 ;
+
+  Game.ground1 = createGround() ;
+  Game.ground1h = Game.groundh  ;
+
+  Game.ground2 = createGround() ;
+  Game.ground2h = Game.groundh  ;
+
+  Game.scrollbarcanvas1 = createScrollBar() ;
+  Game.scrollbarcanvas2 = createScrollBar() ;
+
+  Game.birdcanvas = document.createElement("canvas");
+  Game.birdcanvas.style.position = "absolute" ;
+  Game.birdcanvas.style.left = "0px" ;
+  Game.birdcanvas.style.top = "0px" ;
+  Game.birdcanvas.width = 800 ;
+  Game.birdcanvas.height = 623 ;
+
+  Game.stopAnimation = false ;
+ window.requestAnimationFrame(function(){
+   renderGame();
+  }, Game.canvas);
+  Game.canvascontainer.appendChild(Game.canvas);
+  Game.canvascontainer.appendChild(Game.birdcanvas);
+
+}
+
+
+
+
+function renderGame()
+{
+  var context = Game.canvas.getContext("2d");
+  context.clearRect(0,0,Game.canvas.width, Game.canvas.height);
+  renderScollBar(context);
+  renderGround(context);
+  renderBird();
+
+  if(iscollision())
+  {
+    Game.stopAnimation = true ;
+	window.requestAnimationFrame(function(){
+      renderGameOver();
+    }, Game.canvas);
+	Game.birdcanvas.removeEventListener("click",canvasClick,true);
+  }
+
+  if(!Game.stopAnimation)
+  {
+	  window.requestAnimationFrame(renderGame,Game.canvas);
+  }
+};
+
+function renderGameOver()
+{
+  var context = Game.canvas.getContext("2d");
+  context.clearRect(0,0,Game.canvas.width, Game.canvas.height);
+   context.drawImage(Game.ground2,Game.canvas.width-x2,0);
+   context.drawImage(Game.ground1,Game.canvas.width-x1,0);
+
+  context = Game.birdcanvas.getContext("2d")
+  context.clearRect(0,0,Game.canvas.width, Game.canvas.height);
+  var img = document.createElement("img");
+  img.src= "images/bird2.png";
+  context.drawImage(img, 100,y);
+  if(y < 535)
+  {
+	  y = y+3;
+	  window.requestAnimationFrame(renderGameOver,Game.canvas);
+  }
+}
+
+
+var x1 = 0 ;
+var x2 = -400 ;
+function renderGround(context)
+{
+  if(x1 > Game.canvas.width +70)
+  {
+	  Game.ground1 =  createGround() ;
+	  Game.ground1h = Game.groundh  ;
+	  x1 = 0 ;
+  }
+  context.drawImage(Game.ground1,Game.canvas.width-x1,0);
+  if(x2 > Game.canvas.width +70)
+  {
+	  Game.ground2 =  createGround() ;
+	  Game.ground2h = Game.groundh  ;
+	  x2 = 0 ;
+  }
+  context.drawImage(Game.ground2,Game.canvas.width-x2,0);
+  x1 = x1+3 ;
+  x2 = x2+3 ;
+}
+
+var x3 = 0 ;
+var x4 = 800 ;
+function renderScollBar(context)
+{
+  context.drawImage(Game.scrollbarcanvas1, x3,572);
+  x3 = x3-3 ;
+  context.drawImage(Game.scrollbarcanvas2,x4 ,572);
+  x4 = x4-3 ;
+  if(x3 <-795)
+  {
+    x3 = 800 ;
+  }
+  if(x4 <-795)
+  {
+    x4 = 800 ;
+  }
+};
+
+var y  = 525 ;
+var drop = true ;
+function renderBird()
+{
+  var context = Game.birdcanvas.getContext("2d");
+  context.clearRect(100,y-3,64, 50);
+
+  Game.bird = document.createElement("img");
+  Game.bird.src= "images/bird1.png";
+  context.drawImage(Game.bird, 100,y);
+  if((drop)&&(y < 525))
+  {
+	   y = y+3;
+  }
+  Game.birdcanvas.addEventListener("click",canvasClick,true);
+}
+
+var h = 0 ;
+function canvasClick(evt)
+{
+	h = y-50 ;
+	drop = false ;
+	window.requestAnimationFrame(function(){
+          renderBirdFly();
+    }, Game.bird);
+}
+
+
+function renderBirdFly()
+{
+  var context = Game.birdcanvas.getContext("2d");
+  if(y > h)
+  {
+	 context.clearRect(100,y,64, 50);
+     y = y-4;
+	 Game.bird = document.createElement("img");
+     Game.bird.src= "images/bird1.png";
+     context.drawImage(Game.bird, 100,y);
+	 window.requestAnimationFrame(renderBirdFly,Game.bird);
+  }else{
+	 drop = true ;
+  }
+}
+
+function iscollision()
+{
+	  if((Game.canvas.width-x1 >30)&&(Game.canvas.width-x1 <159)&&((y <=Game.ground1h+49)||(y >=(Game.ground1h+150)))){
+          return true ;
+	  }else if((Game.canvas.width-x2 >30)&&(Game.canvas.width-x2 <159)&&((y <=Game.ground2h+49)||(y >=(Game.ground2h+150)))){
+          return true ;
+	  }else{
+		  return false;
+	  }
+
+}
+
+window.requestAnimationFrame =
+        window.__requestAnimationFrame ||
+                window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                (function () {
+                    return function (callback, element) {
+                        var lastTime = element.__lastTime;
+                        if (lastTime === undefined) {
+                            lastTime = 0;
+                        }
+                        var currTime = Date.now();
+                        var timeToCall = Math.max(1, 33 - (currTime - lastTime));
+                        window.setTimeout(callback, timeToCall);
+                        element.__lastTime = currTime + timeToCall;
+                    };
+                })();
